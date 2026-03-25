62e51b837eed8e210491411633f476ca088f5881
AnnaShunkar <anna.shunkar4@gmail.com>
2026-03-25 16:41:59 +0200

    Add styles and restructure app

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import '../styles/main.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1> <span className="title-deco">KanBan</span> Board</h1>
+        <p className="desritpion">KanBan Board helps teams visualize progress, limit work-in-progress (WIP), and identify
+            bottlenecks, improving efficiency and
+            communication.</p>
+      </header>
+    </>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import App from './app/App.tsx'
 import { AuthProvider } from './context/AuthProvider.tsx'
 import { WorkspaceProvider } from './store/WorkspacesProvider.tsx'
 
