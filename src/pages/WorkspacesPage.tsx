3bfcf47eb9846bf2517003c6e7b9152a2ae32488
AnnaShunkar <anna.shunkar4@gmail.com>
2026-04-03 12:53:55 +0300

    LogOut button and styles

--- src/pages/WorkspacesPage.tsx
+++ src/pages/WorkspacesPage.tsx
@@ -1,12 +1,24 @@
 import type { FC } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 import {AddWorkspaceForm} from "../components/workspaces/AddWorkspaceForm";
 import {WorkspaceList} from "../components/workspaces/WorkspaceList";
 import "../styles/main.css"
+import { AppRoutes } from "../utils/routes";
 
 export const WorkspacesPage: FC = () => {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = (): void => {
+        logout();
+        navigate(AppRoutes.Home);
+    }
     return (
         <section className="workspaces-page">
             <h1 className="workspaces-title">Workspaces</h1>
+            <button type="button" onClick={handleLogout} className="logout-button">
+                Log out
+            </button>
             <div className="workspaces-panel">
                 <AddWorkspaceForm />
                 <WorkspaceList />
