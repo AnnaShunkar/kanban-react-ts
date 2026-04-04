4f9b8238fca2eea80befc5466ead3c638f669b51
AnnaShunkar <anna.shunkar4@gmail.com>
2026-04-04 21:52:18 +0300

    CRUD for tasks

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ColumnsModule } from './columns/columns.module';
+import { TasksModule } from './tasks/task.module';
 
 @Module({
-  imports: [PrismaModule, WorkspacesModule, ColumnsModule],
+  imports: [PrismaModule, WorkspacesModule, ColumnsModule, TasksModule],
 })
 export class AppModule {}
